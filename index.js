c0485bc17b2eceeb79477a9c1fa8397f5fb01ceb
Mareks <m.silmanovics@gmail.com>
2023-01-13 11:43:06 +0100

    commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-document.getElementById("myH").style.color = "red";
-document.getElementById("myP").style.color = "red";
 document.getElementById("myH").style.fontFamily = "Verdana, Arial, sans-serif";
-document.getElementById("myP").style.fontFamily = "Verdana, Arial, sans-serif";
-
 
 
 function myFunction() {
     var x = document.createElement("IMG");
-    document.getElementById("myH").innerHTML = "Кирюша";
-    document.getElementById("myP").innerHTML = "Я Тебя любою ";
-    x.setAttribute("src", "images/red-heart_2764-fe0f.png");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "230");
+    document.getElementById("myH").innerHTML = "ИДИ НАХУЙ";
+    x.setAttribute("src", "images/GwYIge.gif");
+    x.setAttribute("width", "1600");
+    x.setAttribute("height", "800");
     x.setAttribute("alt", "The Pulpit Rock");
+    x.setAttribute("audio", "sound/screamer-test-103648.mp3");
     x.style.display = "block";
     x.style.margin = "0 auto";
     document.getElementById("knapp").style.display = "none";
     document.body.appendChild(x);
   }
+
+  const audio = new Audio();
+  audio.src = "sound/screamer-test-103648.mp3"
